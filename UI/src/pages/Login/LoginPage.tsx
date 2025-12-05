a49388ee94ae82d36de0fe18a8c787cf5efdb138
alvinmoy <alvinsanmoy@gmail.com>
2025-12-05 16:58:22 -0600

    sign in and login updated

--- UI/src/pages/Login/LoginPage.tsx
+++ UI/src/pages/Login/LoginPage.tsx
@@ -61,7 +61,9 @@ function LoginPage() {
   return (
     <div className="login-page">
       <div className="login-card">
-        <h1 className="brand">Do2</h1>
+        <h1 className="brand">
+          <img src="src\assets\logo.png" alt="Do2 Logo" />
+        </h1>
         <h2 className="title">Login</h2>
 
         <form className="login-form" onSubmit={handleLogin}>
@@ -107,7 +109,10 @@ function LoginPage() {
         {data && !data.success && <div className="error">Incorrect email or password provided</div>}
 
         <div className="signup-link">
-          First time here? <a onClick={() => navigate('/signup')}>Sign Up</a>
+          First time here?{' '}
+          <a className="signup-link-button" onClick={() => navigate('/signup')}>
+            Sign Up
+          </a>
         </div>
       </div>
     </div>
